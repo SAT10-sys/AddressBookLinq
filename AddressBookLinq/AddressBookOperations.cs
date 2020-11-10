0004bf5b576d8fa45bf274b6cd6869d44bfaa2e0
Satantra Tewari <satantra1508@gmail.com>
2020-11-11 00:18:21 +0530

    [satantra] data table of address book created

---
+++ AddressBookLinq/AddressBookOperations.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+
+namespace AddressBookLinq
+{
+    public class AddressBookOperations
+    {
+        public DataTable addressBook = new DataTable();
+    }
+}
